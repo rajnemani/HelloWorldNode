3ab9342fcdae51d961b5ec23aaf099536cb489df
rajnemani <rajnemani@gmail.com>
2015-03-15 19:28:28 -0700

    More mods

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var http = require('http');
+
+
+
+var status = "";
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+
+  var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://ec2-54-69-153-27.us-west-2.compute.amazonaws.com:27017/exampleDb", function(err, db) {
+        if(!err) {
+            status+= "We are connected";
+        }
+        else {
+
+            status+= "We are having issues." + err.message;
+        }
+    });
+
+
+  res.end(status+'\n');
 }).listen(5050);
 console.log('Server running on port 5050');
