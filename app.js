1c5d120f744efab1c71b92427a67a54109c32b74
rajnemani <rajnemani@gmail.com>
2015-03-16 18:04:37 -0700

    keeping fixing

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var status = "";
 var address = process.env.MONGODB_PORT_27017_TCP_ADDR;
 var port = process.env.MONGODB_PORT_27017_TCP_PORT;
 status+= "Address: " + address + " Port: " + port;
-
+var connection = "mongodb://" + address + ":" + port + "/exampleDb"
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   var MongoClient = require('mongodb').MongoClient;
 
     // Connect to the db
-    MongoClient.connect("mongodb://ec2-54-69-153-27.us-west-2.compute.amazonaws.com:27017/exampleDb", function(err, db) {
+    MongoClient.connect(connection, function(err, db) {
         if(!err) {
             status+= "We are connected";
             db.createCollection('usercollection', function(err, collection) {});
